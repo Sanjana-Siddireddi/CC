60ed163226de2ce90238cbbf8d63fe8972fa1d63
Siddireddi Shanmukha Sanjana <151708467+Sanjana-Siddireddi@users.noreply.github.com>
2026-03-28 10:10:45 +0530

    7_A.py

---
+++ 7_A.py
@@ -0,0 +1,23 @@
+pip install Flask
+
+____________________
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello():
+	return 'Hello World from Google App Engine (Python)!'
+if __name__ == '__main__':
+	app.run()
+
+__________________________
+
+
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+@app.route('/')
+def time():
+        return "Current Date and Time: " + str(datetime.now())
+if __name__ == '__main__':
+        app.run(port=8080)
